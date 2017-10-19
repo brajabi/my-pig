53003d0bb04061a98d0a6e7814b029b4d3ad2c89
Behnam <b.rajabifard@gmail.com>
2017-10-19 16:15:03 +0330

    fix(releasing): add styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,42 +3,6 @@ import styled from 'styled-components'
 
 import { MenuIcon, MyNotebook } from './icons'
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 50px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 10px 0 10px;
-  background: #eee;
-`
-
-const Title = styled.h2`
-  color: #999;
-  font-weight: 400;
-`
-
-const MenuContainer = styled.div`
-  width: 24px;
-  height: 24px;
-  
-  svg {
-    width: 24px;
-    height: 24px;
-  }
-`
-
-const NotebookContainer = styled.div`
-  width: 30px;
-  height: 30px;
-  
-  svg {
-    width: 30px;
-    height: 30px;
-  }
-`
-
-
 const NavBar = () => (
   <Wrapper>
 
@@ -55,4 +19,39 @@ const NavBar = () => (
   </Wrapper>
 )
 
+const Wrapper = styled.div`
+  width: 100%;
+  height: 50px;
+  flex: 1 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px 0 10px;
+  background: #eee;
+`
+
+const Title = styled.h2`
+  color: #999;
+  font-weight: 400;
+`
+
+const MenuContainer = styled.div`
+  width: 24px;
+  height: 24px;
+
+  svg {
+    width: 24px;
+    height: 24px;
+  }
+`
+
+const NotebookContainer = styled.div`
+  width: 30px;
+  height: 30px;
+
+  svg {
+    width: 30px;
+    height: 30px;
+  }
+`
 export default NavBar;
