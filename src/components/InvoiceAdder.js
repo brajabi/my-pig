e4646ccad9a96e993f4530bda277b04f499596c6
Behnam <b.rajabifard@gmail.com>
2017-10-19 16:14:26 +0330

    feat(releasing): add styles

--- src/components/InvoiceAdder.js
+++ src/components/InvoiceAdder.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 50px;
-  border: 3px solid #2D9AFF;
-  display: flex;
-  align-items: center;
-`
-
 const InviceAdder = ({
   onChangeTitle,
   titleValue,
@@ -17,19 +9,59 @@ const InviceAdder = ({
   onAddClick
 }) => (
     <Wrapper>
-      <input
+      <InvoiceTitleText
         type="text"
         onChange={onChangeTitle.bind(this)}
         value={titleValue}
-        placeholder="item name"
+        placeholder="Invoice name"
       />
-      <input
-        type="text"
+      <InvoicePriceNumber
+        type="number"
+        size="4"
         onChange={onChangePrice.bind(this)}
+        placeholder="Price"
         value={priceValue}
       />
-      <button onClick={onAddClick}>add</button>
+      <ButtonAdder onClick={onAddClick}>+</ButtonAdder>
     </Wrapper>
 )
+
+const Wrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  border-top: 3px solid #2D9AFF;
+  display: flex;
+  align-items: center;
+`
+
+const InvoiceTitleText = styled.input`
+  width: 50%;
+  height: 40px;
+  border: none;
+  padding: 0 0 0 10px;
+  border-right: 1px solid #eee;
+  outline: none;
+`
+
+const InvoicePriceNumber = styled.input`
+  width: 30%;
+  height: 40px;
+  border: none;
+  padding: 0 0 0 10px;
+  border-right: 1px solid #eee;
+  outline: none;
+`
+
+const ButtonAdder = styled.button`
+  width: 20%;
+  height: 47px;
+  border: none;
+  background: #29B6F6;
+  color: #fff;
+  font-size: 35px;
+`
 
 export default InviceAdder
