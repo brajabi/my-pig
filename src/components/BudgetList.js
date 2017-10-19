bf9822d0d0199232572a3ace3ec7fe16a94fdd32
Behnam <b.rajabifard@gmail.com>
2017-10-19 13:16:21 +0330

    feat(releasing): add new components

--- src/components/BudgetList.js
+++ src/components/BudgetList.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
+
 import { add_invoice } from '../actions/invoice'
 import InvoiceItem from './InvoiceItem'
 import InvoiceAdder from './InvoiceAdder'
+import NowTimeLine from './NowTimeLine'
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
+const InvoiceItemContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 20px 0 20px;
+  font-size: 20px;
+`
 
 class BudgetList extends Component {
 
@@ -39,16 +57,16 @@ class BudgetList extends Component {
     });
 
     return (
-      <div>
+      <Wrapper>
 
-        <div>Sum: {totalPrice}</div>
-
-        <ul>
+        <InvoiceItemContainer>
           {valuesArray.map( ( p, i ) => (
             <InvoiceItem title={p.title} price={p.price} key={i}  />
           ))}
-        </ul>
-        
+        </InvoiceItemContainer>
+
+        <NowTimeLine totalPrice={totalPrice} />
+
         <InvoiceAdder
           onChangeTitle={this.itemSaver}
           titleValue={this.state.currentValue}
@@ -56,7 +74,8 @@ class BudgetList extends Component {
           priceValue={this.state.currentPrice}
           onAddClick={this.addInvoiceHandler}
         />
-      </div>
+
+      </Wrapper>
     )
   }
 }
