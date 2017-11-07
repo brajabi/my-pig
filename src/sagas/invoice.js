27e2faab19bd63f2d0c90f6ebc2823cc23cbc391
Behnam <b.rajabifard@gmail.com>
2017-11-07 12:25:37 +0330

    feat(releasign): add new watcher actions saga

--- src/sagas/invoice.js
+++ src/sagas/invoice.js
@@ -1,22 +1,71 @@
-import { put, call, takeEvery, takeLatest } from 'redux-saga/effects'
+import { put, call, takeEvery, takeLatest, fork, select, all } from 'redux-saga/effects'
 import axios from 'axios'
 import * as actions from '../actions/invoice'
+import { stringify } from 'qs'
 
-export function* saveInvoiceItem(action) {
+var instance = axios.create({
+  baseURL: 'http://behnam.ninja/api',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+});
+
+function sendData(Data){
+  return instance({
+    method: 'post',
+    url: '/index.php',
+    data: stringify(Data),
+  })
+  .then(({ data }) => data);
+}
+
+function allInvoiceApi(Data){
+  return instance({
+    method: 'post',
+    url: '/all_invocies.php',
+    data: stringify(Data),
+  })
+  .then(({ data }) => data);
+}
+
+
+export function* fetchInvoiceItems(action) {
   try {
-    const response = yield call( axios.post, 'http://behnam.ninja/api/index.php', {
-      invoiceTitle: 'hi',
-      invoicePrice: 10000,
-    });
+    const response = yield call( allInvoiceApi, {});
+    console.log(response.status);
     
-    console.log(response.data.status);
-
-    yield put(actions.addInvoice( action.title, action.price ));
+    yield put(actions.addInvoiceList( response ));
   } catch(e) {
     yield console.log('error',e);
   }
 }
 
+export function* saveInvoiceItem(action) {
+  try {
+    const response = yield call( sendData, {
+      invoiceTitle: action.title,
+      invoicePrice: action.price
+    });
+    
+    console.log(response.status);
+
+    yield put(actions.addInvoice( response.invoiceTitle, response.invoicePrice ));
+  } catch(e) {
+    yield console.log('error',e);
+  }
+}
+
+export function* watchFetchInvoiceRequest(){
+  yield takeEvery('FETCH_INVOICE_REQUEST', fetchInvoiceItems)
+}
+
 export function* watchAddInvoice(){
-  yield  takeEvery('ADD_INVOICE', saveInvoiceItem)
+  yield takeEvery('ADD_INVOICE_REQUEST', saveInvoiceItem)
+}
+
+export function* root(){
+  yield all([
+    fork(watchAddInvoice),
+    fork(watchFetchInvoiceRequest)
+  ])
 }
