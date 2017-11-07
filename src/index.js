40e916990b04b37b8120753afc8ae08fcd8af7be
Behnam <b.rajabifard@gmail.com>
2017-11-07 10:58:52 +0330

    feat(releasing): add redux-saga middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import createSagaMiddleware from 'redux-saga'
 
-import { watchAddInvoice } from './sagas/invoice'
+import { root } from './sagas/invoice'
 import App from './components/App'
 import rootReducer from './reducer'
 
-const store = createStore(rootReducer)
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware)
+)
+
+sagaMiddleware.run(root)
 
 render(
   <Provider store={store}>
