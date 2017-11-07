14381300a52c4572ced62ab853734d736b02f677
Behnam <b.rajabifard@gmail.com>
2017-11-07 10:59:41 +0330

    feat(releasing): add invoice list object

--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,13 @@
 const InitializeValue = [
-  {title: 'Coffee', price: 10},
-  {title: 'Tea', price: 15},
 ];
 
 function root(state = InitializeValue, action) {
   switch (action.type) {
     case 'ADD_INVOICE':
       return [...state, {title: action.title, price: action.price}]
+
+    case 'ADD_INVOICE_LIST':
+      return [...state, ...action.list]
 
     case 'REMOVE_INVOICE':
       return state
