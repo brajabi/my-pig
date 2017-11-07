bcf766ff9d042ab3ccbb3d042df73a33d5e3cc14
Behnam <b.rajabifard@gmail.com>
2017-11-07 11:02:08 +0330

    feat(releasing): add addInvoiceRequest & addInvoiceList action

--- src/actions/invoice.js
+++ src/actions/invoice.js
@@ -1,7 +1,28 @@
+export const addInvoiceRequest = (title, price) => {
+  return {
+    type: 'ADD_INVOICE_REQUEST',
+    title: title,
+    price: parseInt(price),
+  }
+}
+
 export const addInvoice = (title, price) => {
   return {
     type: 'ADD_INVOICE',
     title: title,
     price: parseInt(price),
+  }
+}
+
+export const addInvoiceList = (invoiceList) => {
+  return {
+    type: 'ADD_INVOICE_LIST',
+    list: invoiceList,
+  }
+}
+
+export const fetchInvoices = () => {
+  return {
+    type: 'FETCH_INVOICE_REQUEST'
   }
 }
